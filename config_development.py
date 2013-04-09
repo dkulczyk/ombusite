84fed37d488dbad2cb706f1d9902c24c85b06471
Jon Glick <jonathanpglick@gmail.com>
2013-04-09 16:25:34 -0700

    Setup blog config, blog and article templates

--- config_development.py
+++ config_development.py
@@ -8,7 +8,7 @@ AUTHOR = u"Martin Rio"
 SITENAME = u"OMBU: Web Development in Portland, Oregon"
 TIMEZONE = 'US/Pacific'
 DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = True
 THEME = 'theme'
 PATH = 'src'
 STATIC_PATHS = ['images']
@@ -16,7 +16,10 @@ FEED_ATOM = None
 FEED_ALL_ATOM = None
 FEED_RSS = None
 PLUGINS = ['pelican.plugins.assets', ]
-DIRECT_TEMPLATES = ('index',)
+DIRECT_TEMPLATES = ('index','blog')
+PAGINATED_DIRECT_TEMPLATES = ('blog',)
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
 from custom_filters import cdn
 JINJA_FILTERS = {'cdn': cdn}
