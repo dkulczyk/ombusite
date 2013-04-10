d2e5e8d9381f0e09a6684ff94fd56a5fd5b40d5e
Jon Glick <jonathanpglick@gmail.com>
2013-04-10 12:08:31 -0700

    Updates configs with blog settings

--- config_development.py
+++ config_development.py
@@ -7,8 +7,9 @@ BUILD_ENV = 'development'
 AUTHOR = u"Martin Rio"
 SITENAME = u"OMBU: Web Development in Portland, Oregon"
 TIMEZONE = 'US/Pacific'
+DELETE_OUTPUT_DIRECTORY = True
 DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = True
+DEFAULT_PAGINATION = 10
 THEME = 'theme'
 PATH = 'src'
 STATIC_PATHS = ['images']
@@ -23,9 +24,10 @@ TAG_FEED_RSS = None
 TRANSLATION_FEED_ATOM = None
 PLUGINS = ['pelican.plugins.assets', ]
 DIRECT_TEMPLATES = ('index','blog')
-PAGINATED_DIRECT_TEMPLATES = ('blog',)
+PAGINATED_DIRECT_TEMPLATES = ('blog')
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+USE_FOLDER_AS_CATEGORY = False
 
 from custom_filters import cdn
 JINJA_FILTERS = {'cdn': cdn}
