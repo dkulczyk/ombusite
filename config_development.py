393a16287e0b0bc9ca7fb320dce55f3a7aa2b39f
Jon Glick <jonathanpglick@gmail.com>
2013-04-11 11:34:33 -0700

    Adds blog toggle to config

--- config_development.py
+++ config_development.py
@@ -9,11 +9,13 @@ SITENAME = u"OMBU: Web Development in Portland, Oregon"
 TIMEZONE = 'US/Pacific'
 DELETE_OUTPUT_DIRECTORY = True
 DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
 THEME = 'theme'
 PATH = 'src'
+ARTICLE_DIR = 'null'
 STATIC_PATHS = ['images']
-FEED_RSS = 'feeds/rss.xml'
+BLOG_ENABLED = False
+FEED_RSS = False
 FEED_ATOM = None
 FEED_ALL_ATOM = None
 FEED_ALL_RSS = None
@@ -23,10 +25,18 @@ TAG_FEED_ATOM = None
 TAG_FEED_RSS = None
 TRANSLATION_FEED_ATOM = None
 PLUGINS = ['pelican.plugins.assets', ]
-DIRECT_TEMPLATES = ('index','blog')
-PAGINATED_DIRECT_TEMPLATES = ('blog')
-ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+DIRECT_TEMPLATES = ('index',)
+PAGINATED_DIRECT_TEMPLATES = ()
+
+if BLOG_ENABLED == True:
+    ARTICLE_DIR = 'posts'
+    FEED_RSS = 'feeds/rss.xml'
+    DEFAULT_PAGINATION = 10
+    DIRECT_TEMPLATES = ('index','blog')
+    PAGINATED_DIRECT_TEMPLATES = ('blog',)
+    ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+    ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
 USE_FOLDER_AS_CATEGORY = False
 
 from custom_filters import cdn
