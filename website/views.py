b6b94c44effb5e851baf8dd0b94b5e682e75976b
Candice Newton <candice.c.newton@gmail.com>
2017-10-06 12:49:36 -0700

    Ticket #26838: Add about page

--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request, context={}):
     context['title'] = "OMBU Website homepage"
     return render(request, 'home.html', context)
+
+def about(request, context={}):
+    context['title'] = "About OMBU Website homepage"
+    return render(request, 'about.html', context)
