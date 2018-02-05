c6ab7bf0f73d78a5a55fe570a8b313cd0f1696fd
Candice Newton <candice.c.newton@gmail.com>
2018-02-05 10:10:47 -0800

    Ticket #29024: Add optional data-img attribute to image overlay links to use different image for modal

--- website/static/website/js/scripts-all.js
+++ website/static/website/js/scripts-all.js
@@ -410,9 +410,17 @@ $(function() {
 
   $('.image-overlay--link').on('click', function(e) {
     e.preventDefault();
-    const imageSource = $(this)
-                          .find('img')
-                          .attr('src');
+    var imageSource;
+
+    // If data-img attribute is present
+    if ($(this).data('img')) {
+      imageSource = $(this).data('img');
+    }
+    // else you image source
+    else {
+      imageSource = $(this).find('img').attr('src');
+    }
+
     $('#image-overlay-modal').find('.image-overlay').attr('src', imageSource);
     $('#image-overlay-modal').modal();
   });
