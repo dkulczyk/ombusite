6fb851a5c9059dd7637c8329b749e9bbefe33fb0
Candice Newton <candice.c.newton@gmail.com>
2018-02-15 11:18:47 -0800

    Ticket #29023: Style project slider for two images of varying aspect ratios to have equal width

--- website/static/website/js/scripts-all.js
+++ website/static/website/js/scripts-all.js
@@ -393,6 +393,23 @@ $(function() {
 // Slider
 $(function() {
 
+  // Determin height of slide--has-two-images slides
+  $('.slider--slide--has-two-images').each(function() {
+    var width1 = $(this).find('.slider--slide-image:first img').width();
+    var height1 = $(this).find('.slider--slide-image:first img').height();
+    var width2 = $(this).find('.slider--slide-image:last img').width();
+    var height2 = $(this).find('.slider--slide-image:last img').height();
+    var ratio1 = width1 / height1;
+    var ratio2 = width2 / height2;
+    var ratioRelationship1 = ratio2 / (ratio1 + ratio2) * 100;
+    var ratioRelationship2 = 100 - ratioRelationship1;
+
+    $(this).find('.slider--slide-image:first').css('height', ratioRelationship1 + '%');
+    $(this).find('.slider--slide-image:last').css('height', ratioRelationship2 + '%');
+
+  });
+  
+  // Initialize slider
   $('.slider').owlCarousel({
     loop:true,
     dots:true,
@@ -401,13 +418,13 @@ $(function() {
       0 : {
         items: 1
       },
-      // breakpoint from 480 up
+      // breakpoint from 768 up
       768 : {
         autoWidth:true,
         items: 4
       }
     } 
-  });  
+  });    
 
   $('.slider').addClass('owl-carousel');
 });
