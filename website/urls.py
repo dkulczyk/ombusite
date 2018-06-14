31d30eb3acba73524192d88e33df876afbecaab4
Adam Page <adamfpage@gmail.com>
2018-06-14 16:13:32 -0700

    Ticket #31772: Exclude project profiles for UO, SERI, and Autodesk until content has been developed.

--- website/urls.py
+++ website/urls.py
@@ -38,9 +38,9 @@ urlpatterns = [
     url(r'^work/compliance-next$', website.views.project_compliancenext, name='project-compliancenext'),
     url(r'^work/metro$', website.views.project_metro, name='project-metro'),
     url(r'^work/metro-pcmt$', website.views.project_metropcmt, name='project-metropcmt'),
-    url(r'^work/seri$', website.views.project_seri, name='project-seri'),
-    url(r'^work/autodesk$', website.views.project_autodesk, name='project-autodesk'),
-    url(r'^work/uo$', website.views.project_uo, name='project-uo'),
+    # url(r'^work/seri$', website.views.project_seri, name='project-seri'),
+    # url(r'^work/autodesk$', website.views.project_autodesk, name='project-autodesk'),
+    # url(r'^work/uo$', website.views.project_uo, name='project-uo'),
 
     # Case Studies
     url(r'^work/nwpp$', website.views.casestudy_nwpp, name='case-study-nwpp'),
