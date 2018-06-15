f87d451d8c45d2a79d1fbfdc62d476772b2e3743
Candice Newton <candice.c.newton@gmail.com>
2018-06-15 14:07:58 -0700

    Ticket #32365: Move navigation overlay to bottom of page content to improve SEO, ensure tab key is trapped in navigation when nav is active

--- website/static/website/js/scripts-all.js
+++ website/static/website/js/scripts-all.js
@@ -359,16 +359,38 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
 
 // Menu
 $(function() {
+
+  // Keep focus in navigation
+  // Adapted from https://github.com/udacity/ud891/blob/gh-pages/lesson2-focus/07-modals-and-keyboard-traps/solution/modal.js  
+
+  // Find the modal and its overlay
+  var modal = document.querySelector('#menu');
+
+  // Listen for and trap the keyboard
+  modal.addEventListener('keydown', trapTabKey);
+
+  // Find all focusable children
+  var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+  var focusableElements = modal.querySelectorAll(focusableElementsString);
+  // Convert NodeList to Array
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  var firstTabStop = focusableElements[0];
+  var lastTabStop = focusableElements[focusableElements.length - 1];
+
   $('.header--menu-toggle').on('click', function(e){
     e.preventDefault();
     $('body').toggleClass('navigation--active');
     $('#menu').toggleClass('navigation--active');
+    // Focus first child
+    setTimeout(function(){ firstTabStop.focus(); }, 300);
   });
 
   $('.header--menu-close').on('click', function(e){
     e.preventDefault();    
     $('body').removeClass('navigation--active');
     $('#menu').removeClass('navigation--active');    
+    $('.header--menu-toggle').focus();
   });
 
   $('.navigation--secondary-link').on('click', function(e) {
@@ -377,7 +399,29 @@ $(function() {
       $('#menu').removeClass('navigation--active');
     }, 1000);
   });
+
+  function trapTabKey(e) {
+    // Check for TAB key press
+    if (e.keyCode === 9) {
+
+      // SHIFT + TAB
+      if (e.shiftKey) {
+        if (document.activeElement === firstTabStop) {
+          e.preventDefault();
+          lastTabStop.focus();
+        }
+
+      // TAB
+      } else {
+        if (document.activeElement === lastTabStop) {
+          e.preventDefault();
+          firstTabStop.focus();
+        }
+      }
+    }
+  }
 });
+
 // Callout fullwidth
 $(function() {
   $('.callout--fullwidth').each(function( index, element ) {
