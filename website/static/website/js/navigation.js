78942efb48bb8c7da996b717462a02618e02c855
Candice Newton <candice.c.newton@gmail.com>
2018-06-15 15:51:09 -0700

    Ticket #32402: Fix overflow rules on active navigation to prevent body scrolling when nav is open

--- website/static/website/js/navigation.js
+++ website/static/website/js/navigation.js
@@ -21,7 +21,7 @@ $(function() {
 
   $('.header--menu-toggle').on('click', function(e){
     e.preventDefault();
-    $('body').toggleClass('navigation--active');
+    $('html').toggleClass('navigation--active');
     $('#menu').toggleClass('navigation--active');
     // Focus first child
     setTimeout(function(){ firstTabStop.focus(); }, 300);
@@ -29,14 +29,14 @@ $(function() {
 
   $('.header--menu-close').on('click', function(e){
     e.preventDefault();    
-    $('body').removeClass('navigation--active');
+    $('html').removeClass('navigation--active');
     $('#menu').removeClass('navigation--active');    
     $('.header--menu-toggle').focus();
   });
 
   $('.navigation--secondary-link').on('click', function(e) {
     setTimeout(function(){  
-      $('body').removeClass('navigation--active');
+      $('html').removeClass('navigation--active');
       $('#menu').removeClass('navigation--active');
     }, 1000);
   });
