285119aea2a73cd5051c1af85f07fd97e87d120c
Adam Page <adamfpage@gmail.com>
2019-02-08 15:55:53 -0800

    Ticket #24261: Refactors project profile content out of view configuration into models.

---
+++ website/models/project.py
@@ -0,0 +1,134 @@
+occ = {
+  'client': 'Oregon Convention Center',
+  'title': 'Project profile for Oregon Convention Center’s website redesign',
+  'visit_url': 'https://www.oregoncc.org/',
+  'visit_text': 'Visit Website',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_occ.html',
+  'intro': '<h6>Making the World’s Most Useful Convention Center Website</h6><p>The Oregon Convention Center is a prominent institution of the West Coast, one of only two LEED Platinum certified convention centers in the US. When the OCC approached OMBU, however, their website had two major failings: it looked woefully outdated and cheap, and the site’s navigation and content were messy and confusing.</p><p>The OCC engaged OMBU in a guided Discovery phase, to figure out how to organize, design, and build a web presence that communicated the OCC’s prestigious yet friendly brand and guide audiences to the information they need. </p><p>OMBU then took the information from Discovery and designed and built a sleek, modern, mobile-friendly and easy-to-navigate website that highlights the </p>',
+  'services': [
+    'Technology Strategy',
+    'User Experience Design',
+    'Systems Integration',
+    'Development',
+    'DevOps'
+  ],
+  'testimonial_image': 'website/images/project-images/occ/aerial-photo@2x.png',
+  'testimonial_quote': 'The OCC website is our front door and delivers exactly what we are – a place where things happen, modern, exciting, sustainable and service oriented.  OMBU created a very successful site for us on time and on budget',
+  'testimonial_attribution': 'Matt Pizzuti, Interim Executive Director',
+  'technologies': [
+    'OMBU Core Drupal CMS',
+    'Responsive design',
+    '60fps UI animations',
+    'Integration with EBMS event system',
+    'Apache Solr faceted search',
+    'Live chat',
+    'High-availability managed DevOps'
+  ],
+  'insights_content_template': 'content/project/insights/_occ.html',
+  'url': '/work/occ',
+}
+
+kaufmanhall = {
+  'client': 'Kaufman Hall',
+  'title': 'Project profile for Kaufman Hall’s website redesign',
+  'visit_url': 'https://www.kaufmanhall.com/',
+  'visit_text': 'Visit Website',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_kaufmanhall.html',
+  'intro': '<h6>Powering the Enterprise</h6><p>Kaufman Hall is a management consulting and enterprise software company with clients around the globe. After expanding their business by acquiring the leading software provider in their field, Kaufman Hall needed a unified web presence with an enterprise content management system (CMS) to support their growth and thought leadership.</p><p>OMBU designed and built an Content Management website featuring a modern design and powerful marketing tools. The site features a filterable resource center for white papers, case studies, webinars, and more, to prove Kaufman Hall’s thought leadership. Flexible layout tools mean the marketing team can spin up new landing pages, complete with lead capture, in minutes instead of weeks. Rich integration with Marketo and Salesforce allows the sales team to prospect and funnel attractive leads into their sales pipeline.</p>',
+  'services': [ 'User Experience Design', 'Visual Design', 'Drupal Content Management Development', 'Data Migration' ],
+  'url': '/work/kaufman-hall',
+}
+
+nwcouncilrtf = {
+  'client': 'NW Council Regional Technical Forum',
+  'title': 'Project profile for NW Council RTF’s website redesign',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_nw-council-rtf.html',
+  'intro': '<h6>Modernizing Interaction with Energy Efficiency Program Development</h6><p>OMBU designed and built the new Regional Technical Forum website for NWPCC. The new website serves a technical audience with a modern and task-oriented visual design, and supports content providers with Content Management functions and integrations behind the scenes. For example, content editors can use assets stored in Box as if they were native files in the CMS.</p><p>NWPCC’s mission required transparency and public input. OMBU conceived and built a discussion component that can be placed anywhere on the new website to engage the public. OMBU is currently working with NWPCC to redesign their main council website.</p>',
+  'visit_url': 'https://rtf.nwcouncil.org/',
+  'visit_text': 'Visit Website',
+  'services': [ 'Technology Strategy', 'User Experience Design', 'Visual Design', 'Enterprise Drupal CMS', 'Accessibility Compliance Audit', 'Migration of Measures', 'Training' ],
+  'technologies': [ 'Enterprise Drupal CMS', 'Box Integration for CMS Asset Library', 'Searchable Measure List with Update History', 'Automated Content Migration Script' ],
+  'url': '/work/nw-council-rtf',
+}
+
+metro = {
+  'client': 'Oregon Metro',
+  'title': 'Project profile for Oregon Metro’s website redesign',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_metro.html',
+  'intro': '<h6>Connecting Millions with Their Regional Government</h6><p>OMBU built a powerful and scalable enterprise Drupal platform for Metro to communicate its mission and services, and to provide online resources to its constituents such as the Find a Recycler search application. Using our rich and intuitive publishing tools, over 150 Metro staff from across the agency can draft, moderate, translate and approve content according to customizable permissions. Thanks to an agile process with predictable iterations, oregonmetro.gov launched on time, smoothly and has scaled gracefully .</p><p>Since launch, Metro has engaged OMBU for several follow-up projects, including a major new constituent engagement application, a map tool to solicit public comments on Metro projects, and enhancements to their news section.</p>',
+  'visit_url': 'https://www.oregonmetro.gov/',
+  'visit_text': 'Visit Website',
+  'services': [ 'Technology Strategy', 'Content Management Architecture', 'Drupal CMS Development', 'Government Systems Integration', 'Training & Rollout' ],
+  'technologies': [ 'Enterprise Drupal CMS', 'Publishing Moderation System', 'Translation/Localization for 13 Languages', 'Responsive Design', 'Rich Interactive Maps', 'Integration with Government Systems', 'Apache Solr Faceted Search', 'Searchable PDF Documents' ],
+  'url': '/work/metro',
+}
+
+metropcmt = {
+  'client': 'Oregon Metro Public Comment Map App',
+  'title': 'Project profile for Oregon Metro PCMT’s web application design',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_metro-pcmt.html',
+  'intro': '<h6>Engaging the Public with Interactive Maps</h6><p>Metro engaged OMBU to design and implement a map-based public comment tool. Metro uses this tool to engage with the community on large transit planning projects, such as the Southwest Corridor and the Powell-Division Transit and Development Project.</p><p>OMBU’s web application allows Metro staff to design project plans and comment points on a modern mapping platform, and request comments from the public on routes and points through comment forms and rich media.</p>',
+  # 'visit_url': 'http://oregonmetro.gov/swcorridormap',
+  # 'visit_text': 'Visit Website',
+  'services': [ 'User Experience Design', 'Visual Design', 'Drupal Application Development', 'Interactive Map Application Development' ],
+  'url': '/work/metro-pcmt',
+}
+
+stand = {
+  'client': 'Stand for Children',
+  'title': 'Project profile for Stand for Children’s organizer app',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_stand.html',
+  'intro': '<h6>Empowering Community Organizers to Effect Change for Students</h6><p>Stand for Children reached out to OMBU to develop an iPad app with an ambitious goal: to transform the way Stand’s field organizers reach, manage, and activate their volunteer base across the nation.</p><p>OMBU’s Discovery determined that, rather than building an iPad-only app that would restrict the audience to these tablets, Stand would be better off with a mobile web app that would work on any device and platform. This early decision meant that, when Stand later switched it’s tablets from Apple to Microsoft, the app we built worked out- of-the-box and didn’t require additional investment.</p><p>The app integrates in real-time with Stand’s Salesforce CRM, which means that app data is always in sync with gift, membership development, and contact data that are critical for Stand’s daily operations.</p><p>The app was rolled out nationally and has become a critical tool for Stand’s efforts to drive parent engagement and improve outcomes for kids.</p>',
+  'services': [ 'Technology Strategy', 'Table Application Design', 'Application Development' ],
+  'technologies': [ 'Angular JavaScript Application', 'Real-time Salesforce Integration', 'Touch-centric UI' ],
+  'url': '/work/stand-for-children',
+}
+
+compliancenext = {
+  'client': 'NAVEX Global <span class="widow-prevention">Compliance Next</span>',
+  'title': 'Project profile for Compliance Next’s website design',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_compliance-next.html',
+  'intro': '<h6>A Community Platform for a Burgeoning New Industry</h6><p>OMBU built the world’s first community-driven compliance think tank for NAVEX Global. Compliance is an emerging and rapidly growing career field with limited resources for members. To help our client seize the opportunity,  OMBU launched Compliance Next in 5-months. The website features learning tracks and resources and is quickly evolving with features that allow peer-to-peer networking.</p>',
+  'visit_url': 'https://www.navexglobal.com/compliancenext/',
+  'visit_text': 'Visit Website',
+  'services': [ 'Services', 'Technology Strategy', 'Application Architecture', 'Application Development', 'Systems Integration' ],
+  'technologies': [ 'Django Community Application Platform' ],
+  'url': '/work/compliance-next',
+}
+
+smithsonian = {
+  'client': 'Smithsonian Institute <span class="widow-prevention">Lemelson Center</span>',
+  'title': 'Project profile for Smithsonian Institute Lemelson Center’s website design',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_smithsonian.html',
+  'intro': '<h6>An Innovative Website for the Museum of Innovation</h6><p>The Smithsonian Institute needed a website unique and advanced enough to represent their Lemelson Center for Invention and Innovation, and OMBU were thrilled to be tasked with this challenge. In addition to a quirky design that showcases the center’s bold, forward-thinking identity, the powerful Drupal website boasts features such as real-time faceted search, branded audio and video players, and fun on-page interactive elements to increase visitor engagement.</p><p>The Lemelson site integrates with other Smithsonian systems, such as an institution-wide calendaring solution, email marketing tools, and even an in-museum exhibit. These integrations make the site a  rst-class citizen in the Smithsonian ecosystem, and keep rich data  owing without staff intervention.</p>',
+  'visit_url': 'http://invention.si.edu/',
+  'visit_text': 'Visit Website',
+  'services': [ 'Technology Strategy', 'Content Management Architecture', 'Content Management Implementation', 'Archive Records Migration', 'Government Systems Integration', 'Training and Rollout' ],
+  'technologies': [ 'Enterprise Drupal CMS', 'Custom Video and Audio Players', 'Migration of 1700+ Archive Records', 'Apache Solr Faceted Search', 'Interactive Canvas Drawing Functionality', '“Surprise Me” Exploration Feature' ],
+  'url': '/work/smithsonian',
+}
+
+seri = {
+  'client': 'Sustainable Electronics Recycling International',
+  'title': 'Project profile for Sustainable Electronics Recycling International’s website redesign',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_seri.html',
+  'visit_url': 'https://sustainableelectronics.org/',
+  'visit_text': 'Visit Website',
+  'url': '/work/seri',
+}
+
+autodesk = {
+  'client': 'Autodesk ReCap',
+  'title': 'Project profile for Autodesk ReCap’s website design',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_autodesk.html',
+  'url': '/work/autodesk',
+}
+
+uo = {
+  'client': 'University of Oregon Lundquist College of Business',
+  'title': 'Project profile for University of Oregon Lundquist College of Business’s website design',
+  'hero_carousel_content_template': 'content/project/hero-carousels/_uo.html',
+  'visit_url': 'https://business.uoregon.edu/',
+  'visit_text': 'Visit Website',
+  'url': '/work/uo',
+}
