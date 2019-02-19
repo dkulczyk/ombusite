2f6bac239046c04c379f0c7f1eab5fd90ed7c5d9
Jon Glick <jonathanpglick@gmail.com>
2019-02-19 14:30:08 -0800

    Disables unneeded middleware.

--- application/website/settings/remote.py
+++ application/website/settings/remote.py
@@ -29,11 +29,11 @@ MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware', # GZip.
     'htmlmin.middleware.HtmlMinifyMiddleware', # Minify HTML.
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware', # View caching.
     'htmlmin.middleware.MarkRequestMiddleware', # View caching.
