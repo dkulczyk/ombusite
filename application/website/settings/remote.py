7bf069c9a4c924b6712f17c1defab24d8b344554
Jon Glick <jonathanpglick@gmail.com>
2019-02-18 16:13:30 -0800

    Temporarily adds debug toolbar for remote (refs #35668)

--- application/website/settings/remote.py
+++ application/website/settings/remote.py
@@ -25,3 +25,33 @@ DOMAIN = os.environ.get('DOMAIN', None)
 
 if os.getenv('ENVIRONMENT_TYPE', None) in ('qa', 'staging'):
     MIDDLEWARE = ['basicauth.middleware.BasicAuthMiddleware',] + MIDDLEWARE
+
+
+
+# Temporary debug toolbar.
+DEBUG = True
+INSTALLED_APPS += ['debug_toolbar',]
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware',] + MIDDLEWARE
+
+def show_toolbar_callback(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': None,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar_callback,
+}
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    # 'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    # 'debug_toolbar.panels.sql.SQLPanel',
+    # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    # 'debug_toolbar.panels.templates.TemplatesPanel',
+    # 'debug_toolbar.panels.cache.CachePanel',
+    # 'debug_toolbar.panels.signals.SignalsPanel',
+    # 'debug_toolbar.panels.logging.LoggingPanel',
+    # 'debug_toolbar.panels.redirects.RedirectsPanel',
+]
